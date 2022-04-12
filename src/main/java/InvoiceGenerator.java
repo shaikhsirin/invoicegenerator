3162d2ea3f9919f5bf5195104b45ce44c6cae886
shaikhsirin <shaikhsirin8080example.com>
2022-04-12 21:45:55 +0530

    Added:- Calculating Fare For Multiple Ride whether it is normal or premium

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -23,4 +23,37 @@ public class InvoiceGenerator {
         }
         return totalFare;
     }
+    //calculating fare for a ride whether it s normal or Premium
+    public double calculateFare(double distance, int time, String type) {
+        if (type.equalsIgnoreCase("Normal")) {
+            double totalFare = distance * MINIMUM_COST_PER_KILOMETER_NORMAL + time * COST_PER_TIME_NORMAL;
+            return Math.max(totalFare, MINIMUM_FARE_NORMAL);
+        }
+        else if (type.equalsIgnoreCase("Premium")) {
+            double totalFare = distance * MINIMUM_COST_PER_KILOMETER_PREMIUM + time * COST_PER_TIME_PREMIUM;
+            return Math.max(totalFare, MINIMUM_FARE_PREMIUM);
+        }
+        else {
+            return 0;
+        }
+    }
+
+    //calculating fare for multiple ride whether it s normal or Premium
+    public double calculateFare(Ride[] rides, String type) {
+        double totalFare = 0.0;
+        if (type.equalsIgnoreCase("Normal")) {
+            for (Ride ride : rides) {
+                totalFare += calculateFare(ride.getDistance(), ride.getTime(), type);
+            }
+            return totalFare;
+
+        }
+        else if (type.equalsIgnoreCase("Premium")) {
+            for (Ride ride : rides) {
+                totalFare += calculateFare(ride.getDistance(), ride.getTime(), type);
+            }
+            return totalFare;
+        }
+        return 0;
+    }
 }
