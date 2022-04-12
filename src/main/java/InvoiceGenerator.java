20786f81574ebb5c71410a3d250db64b8c18c2fe
shaikhsirin <shaikhsirin8080example.com>
2022-04-12 21:37:22 +0530

    Added:- Calculating Fare For Ride

--- src/main/java/InvoiceGenerator.java
+++ src/main/java/InvoiceGenerator.java
@@ -14,5 +14,27 @@ public class InvoiceGenerator {
         double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
         return Math.max(totalFare, MINIMUM_FARE);
     }
+
+    //calculating fare for multiple ride
+    public double calculateTotalFare(Ride[] rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare += calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
+    }
+
+    //calculating fare for a ride whether it s normal or Premium
+    public double calculateFare(double distance, int time, String type) {
+        if (type.equalsIgnoreCase("Normal")) {
+            double totalFare = distance * MINIMUM_COST_PER_KILOMETER_NORMAL + time * COST_PER_TIME_NORMAL;
+            return Math.max(totalFare, MINIMUM_FARE_NORMAL);
+        } else if (type.equalsIgnoreCase("Premium")) {
+            double totalFare = distance * MINIMUM_COST_PER_KILOMETER_PREMIUM + time * COST_PER_TIME_PREMIUM;
+            return Math.max(totalFare, MINIMUM_FARE_PREMIUM);
+        } else {
+            return 0;
+        }
+    }
 }
 
